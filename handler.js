8a3a58efb208473493f612b6e542bc011309e13e
David Wolfand <david@unitedincome.com>
2019-04-21 22:42:49 -0400

    Adding plex logger

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 'use strict';
+var multipart = require('parse-multipart');
 
-module.exports.logger = async(event) => {
+module.exports.jsonLogger = async(event) => {
   console.log('Here is the whole event', JSON.stringify(event, null, 2));
   let body = {status: 'json body not yet parsed'};
   try {
@@ -14,6 +15,33 @@ module.exports.logger = async(event) => {
     body: JSON.stringify({
       message: 'Event logged',
       input: body,
+    }, null, 2),
+  };
+};
+
+
+module.exports.plexLogger = async(event) => {
+  console.log('Here is the whole event', JSON.stringify(event, null, 2));
+  const body = (new Buffer(event.body, 'base64')).toString('ascii');
+  console.log('Decoded Body:', body);
+	const boundary = `--${event.headers['Content-Type'].split('boundary=')[1]}`;
+  console.log('boundary:', boundary);
+  let jsonBody = body
+    .replace(`${boundary}--`, '')
+    .replace(boundary, '')
+    .replace(`Content-Disposition: form-data; name="payload"`, '')
+    .replace(`Content-Type: application/json`, '')
+    .trim();
+  try {
+    jsonBody = JSON.parse(jsonBody);
+  } catch (e) {
+    console.log('error parsing body', e.message);
+  }
+  console.log('Parsed body', JSON.stringify(jsonBody, null, 2));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Event logged',
     }, null, 2),
   };
 };
